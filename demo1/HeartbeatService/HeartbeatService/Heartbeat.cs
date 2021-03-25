462e73178bf9bb50485f80c844ebf0c73efc6d28
Aditi Mahajan <67877125+aditi1230mhjn@users.noreply.github.com>
2021-03-25 14:42:02 +0530

    Simple Heartbeat Service
    Simple window service that keeps the record until its active

---
+++ demo1/HeartbeatService/HeartbeatService/Heartbeat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace HeartbeatService
+{
+    public class Heartbeat
+    {
+        private readonly Timer timer;
+        public Heartbeat()
+        {
+            //repeat interval as 1 second (1000ms)
+            timer = new Timer(1000) { AutoReset = true };
+            timer.Elapsed += TimerElapsed;
+            //An Event is called every one second and the method below is executed.
+
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Users\maditi\source\repos\DemoApp\temp.txt", lines);
+        }
+
+        public void Start()
+        {
+            //Stats the Service
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            //Stops the service
+            timer.Stop();
+        }
+    }
+}
